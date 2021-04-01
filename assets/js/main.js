7f84887131831b4e6998cb5c0b761ffc0d8bdc88
Suraj Verma <surajverma@rentalcars.com>
2021-04-01 21:38:32 +0100

    on load fetch movies and render

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,56 @@
-const onLoad = () => {
+const fetchData = (url) => {
   const functionForJSON = (responseObject) => {
-    // unless you have some logic here do that before you return
+    if (responseObject.status !== 200) {
+      throw new Error("oops");
+    }
     return responseObject.json();
   };
 
   const functionForApplication = (dataFromServer) => {
-    const topMovies = dataFromServer.Search;
-    console.log(topMovies);
+    if (dataFromServer.Error) {
+    } else {
+      const topMovies = dataFromServer.Search;
+
+      const constructMovieCard = (each) => {
+        const card = `<div class="card m-4" style="width: 18rem">
+          <img
+            src="${each.Poster}"
+            class="card-img-top"
+            alt="${each.Title}"
+          />
+          <div class="card-body text-center">
+            <h4 class="card-title">${each.Title}</h4>
+            <p class="card-text text-muted fs-6">${each.Year}</p>
+            <div class="d-grid gap-2">
+              <button class="btn btn-primary" type="button">More Info</button>
+              <button class="btn btn-success" type="button">
+                Add to Favorites
+              </button>
+            </div>
+          </div>
+        </div>`;
+
+        return card;
+      };
+
+      const cards = topMovies.map(constructMovieCard);
+
+      $("#cards-container").append(cards);
+    }
   };
 
   const functionToHandleError = (errorObject) => {
-    // handle your error here according to your application
+    window.location.assign("/404.html");
   };
 
-  fetch("http://www.omdbapi.com/?s=avengers&apikey=ba22d6b7")
+  fetch(url)
     .then(functionForJSON)
     .then(functionForApplication)
     .catch(functionToHandleError);
+};
+
+const onLoad = () => {
+  fetchData("http://www.omdbapi.com/?s=avengers&apikey=ba22d6b7");
 };
 
 $(document).ready(onLoad);
