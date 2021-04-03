a4b3587e4b99c3541c7b2af7548ffe838e230d4c
Suraj Verma <surajverma@rentalcars.com>
2021-04-03 10:32:54 +0100

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,41 @@
+const constructMovieCard = (movie) =>
+  `<div class="card m-4" style="width: 18rem">
+    <img
+      src="${movie.Poster}"
+      class="card-img-top"
+      alt="${movie.Title}"
+    />
+    <div class="card-body text-center">
+      <h4 class="card-title">${movie.Title}</h4>
+      <p class="card-text text-muted fs-6">${movie.Year}</p>
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">More Info</button>
+        <button class="btn btn-success" type="button">
+          Add to Favorites
+        </button>
+      </div>
+    </div>
+  </div>`;
+
+const renderMovieCards = (movies) => {
+  const cards = movies.map(constructMovieCard);
+
+  $("#cards-container").append(cards);
+};
+
 const fetchData = (url) => {
   const functionForJSON = (responseObject) => {
     if (responseObject.status !== 200) {
-      throw new Error("oops");
+      throw new Error("Internal Server Error");
     }
     return responseObject.json();
   };
 
   const functionForApplication = (dataFromServer) => {
     if (dataFromServer.Error) {
+      throw new Error(dataFromServer.Error);
     } else {
-      const topMovies = dataFromServer.Search;
-
-      const constructMovieCard = (each) => {
-        const card = `<div class="card m-4" style="width: 18rem">
-          <img
-            src="${each.Poster}"
-            class="card-img-top"
-            alt="${each.Title}"
-          />
-          <div class="card-body text-center">
-            <h4 class="card-title">${each.Title}</h4>
-            <p class="card-text text-muted fs-6">${each.Year}</p>
-            <div class="d-grid gap-2">
-              <button class="btn btn-primary" type="button">More Info</button>
-              <button class="btn btn-success" type="button">
-                Add to Favorites
-              </button>
-            </div>
-          </div>
-        </div>`;
-
-        return card;
-      };
-
-      const cards = topMovies.map(constructMovieCard);
-
-      $("#cards-container").append(cards);
+      renderMovieCards(dataFromServer.Search);
     }
   };
 
